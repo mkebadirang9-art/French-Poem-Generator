129cc53d1fa0496f247fd8bbc3bb50a52a3fc86e
mkebadirang9-art <mkebadirang9@gmail.com>
2025-10-31 22:47:59 +0200

    added  AI API intergration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-function generatePoem(event){
- event.preventDefault();
-
- new Typewriter("#poem", {
-  strings:"Je marcherai les yeux fixés sur mes pensées",
+function displayPoem(response){
+new Typewriter("#poem", {
+  strings:response.data.answer,
   autoStart: true,
   delay: 1,
   cursor: ""
 });
+
+}
+
+
+
+function generatePoem(event){
+ event.preventDefault();
+let instructionInput = document.querySelector("#user-instructions")
+ let apiKey = "2ab140051tc6a33060fa9bfdb4fbcocf";
+let context =
+           "You are a romantic poem expert and love to write short poems.your mission is to generate four line poem in basic HTML and separate each line with <br /> . make sure you follow user instructions";
+ let prompt =`user instructions:Generate french poems about ${instructionInput.value}`;
+let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log("Generating poem");
+console.log(`prompt :${prompt}`);
+console.log(`context: ${context}`);
+
+
+
+ axios.get(apiUrl).then(displayPoem);
+ 
 
 }
 
