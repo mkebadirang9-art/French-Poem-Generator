af40b375c890215c95e89831b83741dde91f6396
mkebadirang9-art <mkebadirang9@gmail.com>
2025-10-31 21:29:42 +0200

    added basic form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("hello world");
+function generatrePoem(event){
+ event.preventDefault();
+
+ new Typewriter("#poem", {
+  strings:"Je marcherai les yeux fixés sur mes pensées",
+  autoStart: true,
+  delay: 1,
+  cursor: ""
+});
+
+}
+
+let poemFormElement = document.querySelector("poem-generator-form");
+poemFormElement.addEventListener("submit",generatrePoem);
